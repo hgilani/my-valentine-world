717b3e2ec68c5f69e77d95eab16f86277dcf9882
Himanshu Gilani <hgilani@gmail.com>
2026-02-14 09:48:37 -0800

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/my-valentine-world/",
+  base: mode === "production" ? "/my-valentine-world/" : "/",
   server: {
     host: "::",
     port: 8080,
